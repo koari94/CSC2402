b3e4d1abd8d8b79c74f6a349d60e41a1bea9f3d7
koari94 <U1144002@umail.usq.edu.au>
2022-05-04 14:07:56 +1000

    Finished and finalised

--- Assignment2Part2.cpp
+++ Assignment2Part2.cpp
@@ -22,38 +22,6 @@ void printArray(int counter, staff *array)
     }
 }
 
-
-string getChoice(staff *array)
-{
-    int i = 0;
-    switch (array[i].staffProf)
-    {
-        case 1:
-            array[i].staffChoice = " is a Receptionist.";
-            i++;
-            break;
-        case 2:
-            array[i].staffChoice = " is an Administrator.";
-            i++;
-            break;
-        case 3:
-            array[i].staffChoice = " is a Nurse.";
-            i++;
-            break;
-        case 4:
-            array[i].staffChoice = " is a Doctor.";
-            i++;
-            break;
-        default:
-        {
-            array[i].name.clear();
-            cout << "Wrong Entry!" << endl;
-        }
-    }
-    return array[i].staffChoice;
-}
-
-
 int main()
 {
     string name;
@@ -62,44 +30,41 @@ int main()
     int counter = 0;
     staffStruct staff[50];
 
-    
-    
     while (staff[i].name != SENTINEL && counter <= 50)
     {
-        cout <<"Please enter staff name: ";
+        cout <<"Please enter staff name ('done' to finish inputting): ";
         getline(cin, staff[i].name);
-            if (staff[i].name != SENTINEL)
+        if (staff[i].name != SENTINEL)
+        {
+            cout << "Please enter staff profession: ";
+            cin >> staff[i].staffProf;
+            if (staff[i].staffProf == 1)
             {
-                cout << "Please enter staff profession: ";
-                cin >> staff[i].staffProf;
-                if (staff[i].staffProf == 1)
-                {
-                    staff[i].staffChoice = " is a Receptionist.";
-                }
-                else if (staff[i].staffProf == 2)
-                {
-                    staff[i].staffChoice = " is an Administrator.";
-                }
-                else if (staff[i].staffProf == 3)
-                {
-                    staff[i].staffChoice = " is a Nurse.";
-                }
-                else if (staff[i].staffProf == 4)
-                {
-                    staff[i].staffChoice = " is a Doctor.";
-                }
-                else
-                {
-                    staff[i].name.clear();
-                    i--;
-                    cout << "Wrong Entry!" << endl;
-                }
-                // getChoice(staff);
-                cin.ignore();
-                counter++;
-                i++;
+                staff[i].staffChoice = " is a Receptionist.";
             }
-        
+            else if (staff[i].staffProf == 2)
+            {
+                staff[i].staffChoice = " is an Administrator.";
+            }
+            else if (staff[i].staffProf == 3)
+            {
+                staff[i].staffChoice = " is a Nurse.";
+            }
+            else if (staff[i].staffProf == 4)
+            {
+                staff[i].staffChoice = " is a Doctor.";
+            }
+            else
+            {
+                staff[i].name.clear();
+                i--;
+                counter--;
+                cout << "Wrong Entry!" << endl;
+            }
+            cin.ignore();
+            counter++;
+            i++;
+        }
     }
 
     printArray(counter, staff);
