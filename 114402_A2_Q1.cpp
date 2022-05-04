7d58bcee8dbe6a604aa4f4b323d74e0e1cacd2c0
koari94 <U1144002@umail.usq.edu.au>
2022-05-05 00:07:19 +1000

    apparently this will replace something by CRLF idk what that is lol

---
+++ 114402_A2_Q1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+// switch statement used for outputting based on what user entered
+void printEntry (int staffProf, string name)
+{
+    switch (staffProf)
+    {
+        case 1:
+            cout << "Staff " << name << " is a Receptionist." << endl;
+            break;
+        case 2:
+            cout << "Staff " << name << " is an Administrator." << endl;
+            break;
+        case 3:
+            cout << "Staff " << name << " is a Nurse." << endl;
+            break;
+        case 4:
+            cout << "Staff " << name << " is a Doctor." << endl;
+            break;
+        default:
+        {
+            cout << "Wrong Entry!" << endl;
+        }
+    }
+}
+
+int main()
+{
+    string name;
+    string SENTINEL = "done";
+    int staffProf;
+    int count = 0;
+    while (name != SENTINEL && count <= 50)
+    {
+        // due to names commonly holding two words, getline() is used to ensure both are inputted, despite white space
+        cout << "Please enter the staff name, or enter 'done' to finish inputting: ";
+        getline(cin, name);
+
+        if (name != SENTINEL && count <= 50)
+        {
+            cout << "Please enter the profession of " << name << ": ";
+            cin >> staffProf;
+            printEntry(staffProf, name);
+            // due to leftovers in the input buffer, cin.ignore() tails the loop to ensure the name input starts from scratch
+            cin.ignore();
+            count++;
+        }
+    }
+    return 0;
+}
---
+++ 114402_A2_Q2.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+// struct initiated using typedef to ensure it can be passed into a function via reference with no issue
+typedef struct staffStruct
+{
+    string name;
+    string staffChoice;
+    int staffProf;
+}staff;
+// staff variable initialised for array reference
+
+// function to handle displaying outputs
+void printArray(int counter, staff *array)
+{
+    cout << "**********List of staff********" << endl;
+    for (int i = 0; i < counter; i++)
+    {
+        cout << "Staff " << array[i].name << array[i].staffChoice << endl;
+    }
+    cout << "**********End of List**********";
+}
+
+int main()
+{
+    string SENTINEL = "done";
+    int i = 0;
+    int counter = 0;
+    // staff struct passed into an array
+    staffStruct staff[50];
+
+    while (staff[i].name != SENTINEL && counter <= 50)
+    {
+        cout <<"Please enter staff name ('done' to finish inputting): ";
+        getline(cin, staff[i].name);
+        if (staff[i].name != SENTINEL)
+        {
+            cout << "Please enter the profession of " << staff[i].name << ": ";
+            cin >> staff[i].staffProf;
+
+            // janky else if statements to handle mapping strings to the integer choice the easiest
+            if (staff[i].staffProf == 1)
+            {
+                staff[i].staffChoice = " is a Receptionist.";
+            }
+
+            else if (staff[i].staffProf == 2)
+            {
+                staff[i].staffChoice = " is an Administrator.";
+            }
+
+            else if (staff[i].staffProf == 3)
+            {
+                staff[i].staffChoice = " is a Nurse.";
+            }
+
+            else if (staff[i].staffProf == 4)
+            {
+                staff[i].staffChoice = " is a Doctor.";
+            }
+
+            else
+            {
+                // in case of wrong input, clears the name input so the loop can start fresh 
+                staff[i].name.clear();
+                i--;
+                counter--;
+                // regressing integer and counter to ensure the wrong input is overwritten
+                // also for some reason if counter doesn't regress it will display the 'done' sentinel in the finished display
+                cout << "Wrong Entry!" << endl;
+            }
+            cin.ignore();
+            counter++;
+            i++;
+        }
+    }
+
+    printArray(counter, staff);
+    return 0;
+}
